dca4870952b920e98c503879f275d17d67541f89
koter at home <42820377+vbaaccess@users.noreply.github.com>
2022-04-10 00:30:05 +0200

    Binding sendMessage function to all buttons.

--- battlegame.py
+++ battlegame.py
@@ -3,6 +3,7 @@ from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 
 import battleships
+from gamebutton import GameButton
 
 
 class TheGame(GridLayout):
@@ -10,8 +11,17 @@ class TheGame(GridLayout):
     def __init__(self, **kwargs):
         super(TheGame, self).__init__(**kwargs)
         # self.ids['opponent'].disabled = True
+
+        # for obj_battleships in self.children[1].children[0].children:
+        #     print(type(obj_battleships))
+
         for c1 in self.children:
-            print(type(c1))
+            for c2 in c1.children:
+                for c3 in c2.children:
+                    for c4 in c3.children:
+                        for c5 in c4.children:
+                            if isinstance(c5, GameButton):
+                                c5.sendMessage = self.sendMessage
 
     def startButtonClick(self):
         # self.ids['oid_GridBattleShips'].ids['player'].disabled = True
@@ -20,6 +30,10 @@ class TheGame(GridLayout):
         self.ids['oid_gameStartButton'].disabled = True
 
         print("Click Button START")
+
+    def sendMessage(self, message):
+        # print(__name__, '=>', message)
+        print(' sendMessage =>', message)
 
 class BattleGameApp(App):
 
