64dc641ba0ab71ac1cd390875da1f939bbe669f2
koter at home <42820377+vbaaccess@users.noreply.github.com>
2022-04-11 22:51:32 +0200

    Refatoring - moving implemented function and UI definitions to the appropriate files

--- battleships.py
+++ battleships.py
@@ -1,17 +1,48 @@
+from kivy import Config
+from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
-import plane
 from gamebutton import GameButton
 
+import plane
 
 class BattleShips(GridLayout):
 
     def __init__(self, **kwargs):
         super(BattleShips, self).__init__(**kwargs)
-        # print('konstruktora BattleShips')
+        print('constructor BattleShips')
+        self.ids['opponent'].disabled = True
 
-    def isShip(self, x, y):
-        return self.ids['player'].ids[y].ids[y].isShip
+        for c1 in self.children:
+            for c2 in c1.children:
+                for c3 in c2.children:
+                    for c4 in c3.children:
+                        if isinstance(c4, GameButton):
+                            c4.sendMessage = self.sendMessage
+
+    def startButtonClick(self):
+        self.ids['player'].disabled = True
+        self.ids['opponent'].disabled = False
+
+        self.ids['oid_gameTextInput'].disabled = True
+        self.ids['oid_gameStartButton'].disabled = True
+
+        print("Click Button START")
 
     def onMessage(self, message):
-        # print(__name__, '=>', message)
-        print(' onMessage =>', message)
+        pass
+
+    def sendMessage(self, message):
+        print(' sendMessage =>', message)   # send message to server
+
+    def isShip(self, x, y):
+        return self.ids['player'].ids[y].ids[x].isShip
+
+
+class BattleShipsApp(App):
+
+    def build(self):
+        return BattleShips()
+
+if __name__ == '__main__':
+    Config.read('config.ini')
+    BattleShipsApp().run()
