c1d1414ed5d5952684ca4058082d72f5ab222a71
koter at home <42820377+vbaaccess@users.noreply.github.com>
2022-04-30 02:17:19 +0200

    Cleaning

--- backend/main.py
+++ backend/main.py
@@ -6,14 +6,13 @@ from backend.game import Game
 
 class Server:
 
-    clients = {}            # dziennik klientow
     games = {}              # dziennik gier
     websocketToGame = {}    # weboscke, ID Gry
 
     gameKeys = ['A', 'B', 'C']
 
     def __init__(self):
-        self.clients = {}
+        print('Constructor')
 
     async def echo(self, websocket, path):
         try:
@@ -41,34 +40,6 @@ class Server:
                 if len(game.players) == 2:
                     await game.handle(websocket, message)
 
-                # if websocket is self.websocketToGame:
-                #     game = self.websocketToGame[websocket]
-                #     if len(game.players) == 2:
-                #         await game.handle(websocket, message)
-                # else:
-                #     self.websocketToGame[websocket] = self.gameKeys[round(len(self.websocketToGame) / 2)]
-                #     if self.websocketToGame[websocket] in self.games:
-                #         self.games[self.websocketToGame[websocket]].add_player(websocket)
-                #     else:
-                #         self.games[self.websocketToGame[websocket]] = Game()
-                #         self.games[self.websocketToGame[websocket]].add_player(websocket)
-
-
-                # # dodanie nowego wpisu jesli nie istnieje
-                # if websocket not in self.clients:
-                #     self.clients[websocket] = True
-
-                # # wyslanie wiadomosci do wszystkich klientow z wylaczeniem nadawcy
-                # # wpis: rozsylam otrzymana wiadomosc do pozostalych
-                # #   klucz
-                # if len(self.clients) > 1:
-                #     print('Forwarding the message:')
-
-                # for client_ws in self.clients:
-                #     if client_ws == websocket:
-                #         continue
-                #     print(f' to {client_ws}: ', message)
-                #     await client_ws.send(message)
         except RuntimeError:
             print('Server.echo Error')
 
